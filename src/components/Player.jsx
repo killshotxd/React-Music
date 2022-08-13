7dbf19882e24f6a2fcd520eee6bec40baf70c1df
killshotxd <hassanansari211@gmail.com>
2022-08-13 18:31:15 +0530

    fixing headings

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,11 +6,13 @@ const Player = () => {
   return (
     <div>
       <div className="player-card">
+        <h3 className="active-song-name">Song Name</h3>
+        <h5 className="active-artist-name">Artist</h5>
         <div className="control-icon">
-          <ImPrevious color="white" size={50} className="icons" />
-          <GoPlay color="#ff5722" size={80} className="icons" />
-          <BsPauseCircleFill color="#ff5722" size={70} className="icons" />
-          <ImNext color="white" size={50} className="icons" />
+          <ImPrevious color="white" size={40} className="icons" />
+          <GoPlay color="#ff5722" size={70} className="icons" />
+          <BsPauseCircleFill color="#ff5722" size={60} className="icons" />
+          <ImNext color="white" size={40} className="icons" />
         </div>
       </div>
     </div>
