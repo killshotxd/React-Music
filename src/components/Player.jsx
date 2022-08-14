beaa7f9eeb5d310772735e965cb4de41a5dcf353
killshotxd <hassanansari211@gmail.com>
2022-08-14 09:34:31 +0530

    toggle play btn

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import { GoPlay } from "react-icons/go";
 import { BsPauseCircleFill } from "react-icons/bs";
 import { ImPrevious, ImNext } from "react-icons/im";
 const Player = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying);
+  };
   return (
     <div>
       <div className="player-card">
@@ -10,8 +15,22 @@ const Player = () => {
         <h5 className="active-artist-name">Artist</h5>
         <div className="control-icon">
           <ImPrevious color="white" size={40} className="icons" />
-          <GoPlay color="#ff5722" size={70} className="icons" />
-          <BsPauseCircleFill color="#ff5722" size={60} className="icons" />
+          {isPlaying ? (
+            <BsPauseCircleFill
+              color="#ff5722"
+              size={60}
+              className="icons"
+              onClick={togglePlay}
+            />
+          ) : (
+            <GoPlay
+              color="#ff5722"
+              size={60}
+              className="icons"
+              onClick={togglePlay}
+            />
+          )}
+
           <ImNext color="white" size={40} className="icons" />
         </div>
       </div>
