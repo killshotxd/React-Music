3d7550dd8f61cf52c5a21e9caf4f9e6dfb59ebd2
killshotxd <hassanansari211@gmail.com>
2022-08-16 09:29:37 +0530

    fixed app crash when reaching end song

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,45 @@ import React, { useState } from "react";
 import AudioFiles from "./components/AudioFiles";
 import { audios } from "./AudioData";
 import Player from "./components/Player";
+
 const App = () => {
   const [songs, setSongs] = useState(audios);
-  const [currentIndex, setCurrentIndex] = useState(null);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const [currentSong, setCurrentSOng] = useState(songs[0]);
+
+  const checkNumber = (number) => {
+    if (number > songs.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return songs.length - 1;
+    }
+    return number;
+  };
+
   const getSongData = (song, index) => {
     setCurrentIndex(index);
     setCurrentSOng(song);
   };
 
   const nextSong = () => {
-    setCurrentIndex(currentIndex + 1);
-    setCurrentSOng(audios[currentIndex + 1]);
+    setCurrentIndex((currentIndex) => {
+      let newIndex = currentIndex + 1;
+
+      return checkNumber(newIndex);
+    });
+
+    setCurrentSOng(audios[currentIndex]);
   };
 
   const prevSong = () => {
-    setCurrentIndex(currentIndex - 1);
-    setCurrentSOng(audios[currentIndex - 1]);
+    setCurrentIndex((currentIndex) => {
+      let newIndex = currentIndex - 1;
+      return checkNumber(newIndex);
+    });
+    setCurrentSOng(audios[currentIndex]);
   };
 
   return (
